d9e049970e9dab4aa8482a41c3d39a87677aeeda
dunnywei <wei_don@yahoo.com>
2017-12-05 21:05:13 -0500

    two dimension array

--- test2.cs
+++ test2.cs
@@ -1,10 +1,11 @@
 //test array
 
-namesapce ArrayAppliation{
+namespace ArrayAppliation{
   
   class Myarray{
      static void Main(string[] args)
      {
+       /*
        int []n=new int[10];
        
        for(int i=0;i<10;i++)
@@ -15,6 +16,25 @@ namesapce ArrayAppliation{
        foreach(int j in n)
        {
           Console.WriteLine("Element[{0}]={1}",i,j);
+       }
+       */
+       
+       //2D arrays
+       
+       int [,] arr1=new int(3,4)
+       {
+         {0,1,2,3},
+         {5,6,7,8},
+         {9,10,11,12}
+         {13,14,15,16}
+       }
+       
+       for(int i=0;i<3;i++)
+       {
+           for(int j=0;j<4;j++)
+           {
+               Console.WriteLine("a[{0},{1}] = {2}",i,j,arr1[i,j]);
+           }
        }
      }
   }
