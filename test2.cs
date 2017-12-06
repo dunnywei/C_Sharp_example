b7a3c0713fd38831dee51bab613721f9f97b436a
dunnywei <wei_don@yahoo.com>
2017-12-05 19:48:00 -0500

    writing array

--- test2.cs
+++ test2.cs
@@ -1,6 +1,23 @@
 //test array
 
 namesapce ArrayAppliation{
+  
+  class Myarray{
+     static void Main(string[] args)
+     {
+       int []n=new int[10];
+       
+       for(int i=0;i<10;i++)
+       {
+         n[i]=i+10;
+       }
+       
+       foreach(int j in n)
+       {
+          Console.WriteLine("Element[{0}]={1}",i,j);
+       }
+     }
+  }
 
 
 }
